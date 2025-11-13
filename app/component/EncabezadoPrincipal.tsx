4d6e1d7510ad245fba916b7fe3dc4253d89fee76
Sol Zito <solzito.1889@gmail.com>
2025-11-13 17:51:59 -0300

    encabezado principal

---
+++ app/component/EncabezadoPrincipal.tsx
@@ -0,0 +1,11 @@
+export const EncabezadoPrincipal = () => {
+  return (
+    <div className="flex flex-col items-center p-2 font-bold">
+      <h1 className=" capitalize text-clip">
+        🏋️Tu compañero digital para una vida sana y activa.
+      </h1>
+      <div className="w-full border border-gray-200 m-2"></div>
+      <h2 className="capitalize text-bold">Entrena con Coach Agustin💪🏻</h2>
+    </div>
+  );
+};
