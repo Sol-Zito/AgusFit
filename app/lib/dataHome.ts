1b69618ad181095909d0ef5008722d79a56a606d
Sol Zito <solzito.1889@gmail.com>
2025-12-02 19:42:31 -0300

    mock para cliente

---
+++ app/lib/dataHome.ts
@@ -0,0 +1,15 @@
+export const descriptionOfFit = {
+  describe:
+    "Estoy para ayudarte a alcanzar tus objetivos de salud y bienestar de forma personalizada y efectiva. Brindandote:",
+  objects: [
+    "Programas de entrenamiento adaptados a distintos niveles y objetivos, tanto para el gimnasio como para tu hogar.",
+    "Videos explicativos y descripciones detalladas de cada ejercicio para que entrenes con seguridad y técnica.",
+    "Rutinas personalizadas según tus metas, disponibilidad y preferencias.",
+    "Planes de alimentación pensados para complementar tu entrenamiento y mejorar tu rendimiento.",
+    "Consejos prácticos sobre hábitos saludables, recuperación, motivación y estilo de vida fit.",
+    "Seguimiento cada 15 días de avances y tecnicas",
+    "Asesoramiento diario de lunes a lunes.",
+  ],
+  ending:
+    "Ya sea que estés empezando o buscando llevar tu entrenamiento al siguiente nivel, te acompaño en cada paso del camino. Nunca es tarde💪🏻",
+};
