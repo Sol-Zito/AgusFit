24db36e5537eac18fc096f1f066ff4e55dadc8b3
Sol Zito <solzito.1889@gmail.com>
2025-12-02 19:41:06 -0300

    cambios para input-password

--- app/pages/firtsSteps/first.tsx
+++ app/pages/firtsSteps/first.tsx
@@ -4,6 +4,7 @@ import * as Yup from "yup";
 import { useNavigate } from "react-router";
 import { FormContact } from "./FormContact";
 import { EncabezadoPrincipal } from "~/component/EncabezadoPrincipal";
+import { CiLock, CiUnlock } from "react-icons/ci";
 
 export function First() {
   const [isClient, setIsClient] = useState(true);
@@ -16,6 +17,8 @@ export function First() {
       .oneOf(["Agusfit"], "El valor no es el esperado")
       .required("La contraseña es requirida"),
   });
+
+  const [isPassword, setIsPassword] = useState(true);
 
   return (
     <div className="flex flex-col items-center justify-center pb-4 gap-9 w-5/6 m-auto h-full">
@@ -39,25 +42,39 @@ export function First() {
             errors,
             touched,
             handleChange,
-            handleBlur,
             handleSubmit,
             isSubmitting,
           }) => (
             <form onSubmit={handleSubmit}>
               <label htmlFor="passwordClient">Ingresa la contraseña:</label>
-              <div className="my-2 mx-auto">
+              <div className="my-2 mx-auto static flex justify-end">
                 <input
-                  type="text"
+                  type={isPassword ? "password" : "text"}
                   id="passwordClient"
                   name="passwordClient"
+                  autoFocus
                   onChange={handleChange}
                   value={values.passwordClient}
-                  className="w-full block rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                  maxLength={15}
+                  className="w-full inline-block rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
                 />
+
+                <button
+                  type="button"
+                  onClick={() => setIsPassword(!isPassword)}
+                  className="cursor-pointer py-2 absolute "
+                >
+                  {isPassword ? (
+                    <CiLock className="size-6" />
+                  ) : (
+                    <CiUnlock className="size-6" />
+                  )}
+                </button>
+              </div>
+
                 <p className="text-error">
                   {touched.passwordClient && errors.passwordClient}
                 </p>
-              </div>
               <button
                 type="submit"
                 className="transition duration-150 ease-in-out cursor-pointer hover:-translate-y-1 pointer-events-auto border border-gray-200 hover:bg-primary hover:text-text hover:font-bold 
