064d83be1cff4ae56cf8db338f21002ec7c30ca5
Sol Zito <solzito.1889@gmail.com>
2025-12-04 16:02:42 -0300

    card donde muestra la base del ejercicio

---
+++ app/component/Card/CardSimple.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router";
+import type { DataSimpleExercise } from "~/utils/dataexercise";
+
+export const CardSimple = (props: DataSimpleExercise) => {
+  return (
+    <Link
+      to={`/exercisescomplete/${props.exerciseId}`}
+      className="grid justify-center p-8 m-2 border border-primary"
+      key={props.exerciseId}
+    >
+      <h1 className="mb-2 text-center text-primary antialiased font-semibold tracking-wide underline underline-offset-4">
+        {props.name.toLocaleUpperCase()}
+      </h1>
+      <img src={props.gifUrl} alt={props.name} />
+    </Link>
+  );
+};
