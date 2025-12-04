e86fa78a293c090323c08352e1f5a5ed10f4eae5
Sol Zito <solzito.1889@gmail.com>
2025-12-04 17:00:05 -0300

    se agregan rutas para paginacion

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,10 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("/firtsstep", "routes/firstStep.tsx"),
+  route("/welcome", "routes/principal.tsx"),
+  route("/contactme", "routes/contact.tsx"),
+  route("/exercisesBodyPart/:id", "routes/exercise.tsx"),
+  route("/exercisescomplete/:id", "routes/exerciseComplete.tsx"),
+] satisfies RouteConfig;
