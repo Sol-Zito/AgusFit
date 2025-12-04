25d1c74f53b73102cdfb88473d8ee879474cc32d
Sol Zito <solzito.1889@gmail.com>
2025-12-04 16:59:20 -0300

    funciones para obtener info de api

---
+++ app/lib/GetData.ts
@@ -0,0 +1,83 @@
+import type {
+  DataApiExercise,
+  DataApiExerciseById,
+  DataExercise,
+} from "~/utils/dataexercise";
+
+export type propsDataExercises = {
+  bodyPart: string;
+  url?: string | null;
+};
+
+export const getDataExercisesBodyPart = async ({
+  bodyPart,
+  url,
+}: propsDataExercises): Promise<DataApiExercise> => {
+  const urlDinamic: string =
+    url ??
+    `https://www.exercisedb.dev/api/v1/bodyparts/${bodyPart}/exercises?offset=0&limit=20`;
+
+  try {
+    const response = await fetch(urlDinamic);
+
+    if (!response.ok) {
+      throw new Error("Error fetching products");
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
+
+export type propsDataExercisesMuscle = {
+  muscle: string;
+  url?: string | null;
+};
+
+export const getDataExercisesByMuscle = async ({
+  muscle,
+  url,
+}: propsDataExercisesMuscle): Promise<DataApiExercise> => {
+  const urlDinamic: string =
+    url ??
+    `https://www.exercisedb.dev/api/v1/muscles/${muscle}/exercises?offset=0&limit=20&includeSecondary=false`;
+
+  try {
+    const response = await fetch(urlDinamic);
+
+    if (!response.ok) {
+      throw new Error("Error fetching products");
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
+
+export const getDataExercisesById = async (
+  idExercise: string
+): Promise<DataExercise> => {
+  const urldata = `https://www.exercisedb.dev/api/v1/exercises/${idExercise}`;
+  try {
+    const response = await fetch(urldata);
+
+    if (!response.ok) {
+      throw new Error("Error fetching products");
+    }
+    const dataApi = await response.json();
+    console.log("dataApi al obtener ejercicio por id", dataApi);
+
+    return dataApi.data;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
