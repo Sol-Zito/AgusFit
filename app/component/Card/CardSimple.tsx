07e3479f579c7be2b43c8f5bd57e102be669efce
Sol Zito <solzito.1889@gmail.com>
2025-12-09 11:57:36 -0300

    cardsimple

--- app/component/Card/CardSimple.tsx
+++ app/component/Card/CardSimple.tsx
@@ -5,13 +5,13 @@ export const CardSimple = (props: DataSimpleExercise) => {
   return (
     <Link
       to={`/exercisescomplete/${props.exerciseId}`}
-      className="grid justify-center p-8 m-2 border border-primary"
+      className="grid justify-center p-8 m-2 border border-primary text-center"
       key={props.exerciseId}
     >
       <h1 className="mb-2 text-center text-primary antialiased font-semibold tracking-wide underline underline-offset-4">
         {props.name.toLocaleUpperCase()}
       </h1>
-      <img src={props.gifUrl} alt={props.name} />
+      <img src={props.gifUrl} alt={props.name} className="place-self-center" />
     </Link>
   );
 };
