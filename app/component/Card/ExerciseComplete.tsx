bf850cdd3df8a85fb7311baa16e9d8b392a17085
Sol Zito <solzito.1889@gmail.com>
2025-12-10 15:26:36 -0300

    componente con ejercicio completo

--- app/component/Card/ExerciseComplete.tsx
+++ app/component/Card/ExerciseComplete.tsx
@@ -1,28 +1,34 @@
-// contenedor de esta card tiene q tener el fetch que toma el ejercicio de
-// la api y le pasa el json con la data necesaria
-
 import type { DataExercise } from "~/utils/dataexercise";
-
-// card q muestra el ejercicio con detalles y demas
 
 export const ExerciseComplete = (data: DataExercise) => {
   return (
-    <div key={data.exerciseId}>
-      <h1>{data.name}</h1>
-      <img src={data.gifUrl} alt={data.gifUrl} />
-      <p>Musculo Objetivo: {data.targetMuscles}</p>
-      <p>Parte del cuerpo: {data.bodyParts[0]}</p>
-      <p>Equipo: {data.equipments[0]}</p>
-      <div>
-        <p>Musculo secundario:</p>
-        <ul>
+    <div
+      key={data.exerciseId}
+      className="border p-2 grid grid-rows-auto gap-2 my-5 max-w-max mx-auto"
+    >
+      <div className="text-center  w-4/5 m-auto">
+        <h1>{data.name.toLocaleUpperCase()}</h1>
+        <img
+          src={data.gifUrl}
+          alt={data.gifUrl}
+          className="place-self-center mt-2"
+        />
+      </div>
+      <div className="grid md:grid-cols-2 max-w-max m-auto sm:grid-cols-1">
+        <p>♦️ Musculo Objetivo: {data.targetMuscles}</p>
+        <p>♦️ Parte del cuerpo: {data.bodyParts[0]}</p>
+        <p>♦️ Equipo: {data.equipments[0]}</p>
+        <div>
+          <p className="inline">♦️ Musculo secundario: </p>
           {data.secondaryMuscles.map((sm, ind) => (
-            <li key={ind}>{sm}</li>
+            <p key={ind} className="inline">
+              {sm},{" "}
+            </p>
           ))}
-        </ul>
+        </div>
       </div>
       <div>
-        <p>Instrucciones:</p>
+        <p className="underline">Instrucciones:</p>
         <ul>
           {data.instructions.map((sp, ind) => (
             <li key={ind}>{sp}</li>
