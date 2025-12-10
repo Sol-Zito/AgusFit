be03c01e4e49562288af104b529957d6760d6602
Sol Zito <solzito.1889@gmail.com>
2025-12-10 16:37:06 -0300

    paginacion

--- app/pages/Exercises/ExerciseMuscles.tsx
+++ app/pages/Exercises/ExerciseMuscles.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import { BiArrowBack } from "react-icons/bi";
-import { Link } from "react-router";
 import ButtonToHome from "~/component/Button/ButtonToHome";
 import { CardSimple } from "~/component/Card/CardSimple";
 import { PrincipalHeader } from "~/component/PrincipalHeader";
@@ -18,6 +16,22 @@ export function ExerciseByMuscles({ data }: props) {
   const dataObtenida: propsDataExercisesMuscle = { muscle: data };
   const [dataExercises, setDataExercise] = useState<[DataExercise]>();
   const [apiDataPag, setApiDataPag] = useState<DataApiExercise["metadata"]>();
+
+  const NextPage = async (urlParam: string | null) => {
+    const dataObtenida: propsDataExercisesMuscle = {
+      url: urlParam,
+      muscle: "",
+    };
+    try {
+      const response = await getDataExercisesByMuscle(dataObtenida);
+      const dataApi: DataApiExercise = response;
+      setDataExercise(dataApi.data);
+      setApiDataPag(dataApi.metadata);
+      console.log("se cambio de pagina");
+    } catch (error) {
+      alert("Error fetching data:");
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -51,7 +65,35 @@ export function ExerciseByMuscles({ data }: props) {
           />
         ))}
       </div>
-      <div></div>
+      {/* paginacion */}
+      <div className="mx-auto my-2 w-full text-center flex gap-6 items-center justify-center">
+        <button
+          disabled={apiDataPag?.previousPage ? false : true}
+          type="submit"
+          onClick={() => NextPage(apiDataPag?.previousPage ?? null)}
+          className={
+            apiDataPag?.previousPage
+              ? "transition duration-150 ease-in-out cursor-pointer hover:-translate-y-1 pointer-events-auto border border-gray-200 hover:bg-primary hover:text-text hover:font-bold rounded-md px-3 py-2 font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 mt-2"
+              : "transition duration-150 ease-in-out  cursor-pointer hover:-translate-y-1 pointer-events-auto border border-text bg-text rounded-md px-3 py-2 font-semibold text-white"
+          }
+        >
+          Previous Exercises
+        </button>
+        <button
+          disabled={apiDataPag?.nextPage ? false : true}
+          type="submit"
+          onClick={() =>
+            NextPage(apiDataPag?.nextPage ? apiDataPag?.nextPage : null)
+          }
+          className={
+            apiDataPag?.nextPage
+              ? "transition duration-150 ease-in-out cursor-pointer hover:-translate-y-1 pointer-events-auto border border-gray-200 hover:bg-primary hover:text-text hover:font-bold rounded-md px-3 py-2 font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 mt-2"
+              : "transition duration-150 ease-in-out  cursor-pointer hover:-translate-y-1 pointer-events-auto border border-text bg-text rounded-md px-3 py-2 font-semibold text-white"
+          }
+        >
+          Next Exercises
+        </button>
+      </div>
     </div>
   );
 }
